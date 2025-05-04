c8ac127b1f27d1c1371b7ca7094c4a72c1ce0a72
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-04 23:57:39 +0200

    Ajout du composant RootLayout avec la configuration des métadonnées, des polices locales et des fournisseurs de contexte pour le projet Next.js

---
+++ app/(client)/layout.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { cn } from "@/src/utils/tailwind_cn";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "@/src/components/themes/theme-provider";
+import { ViewportLayout } from "next/dist/lib/metadata/types/extra-types";
+import StoreProvider from "@/src/stores/storesProvider";
+import Navbar from "@/src/blocks/Navbar/Navbar";
+import { SidebarProvider } from "@/src/components/ui/sidebar";
+import { AppSidebar } from "@/src/blocks/dashboardBlocks/app-sidebar";
+import { Toaster } from "react-hot-toast";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Digital 07",
+  description: "Digital 07 Portfolio",
+  icons: "images/Frame 1668.png",
+};
+
+export const viewport: ViewportLayout = {
+  width: "device-width",
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning={true}>
+      <body
+        className={cn(
+          geistSans.variable,
+          geistMono.variable,
+          "antialiased mx-auto relative"
+        )}
+        suppressHydrationWarning={true}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SessionProvider>
+            <StoreProvider>
+              <SidebarProvider>
+                <AppSidebar />
+                <Toaster
+                  position="top-center"
+                  toastOptions={{ duration: 3000 }}
+                />
+                {children}
+              </SidebarProvider>
+            </StoreProvider>
+          </SessionProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
