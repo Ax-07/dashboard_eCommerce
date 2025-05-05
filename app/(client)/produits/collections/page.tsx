2c48c160b256853d662d6e900b890355bac8e890
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-05 15:31:52 +0200

    Ajout de la page Collections pour afficher les collections de produits

---
+++ app/(client)/produits/collections/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CollectionsPage: React.FC = () => {
+    return (
+        <div>
+            <h1>
+                Collections de produits
+            </h1>
+        </div>
+    );
+};
+
+export default CollectionsPage;
