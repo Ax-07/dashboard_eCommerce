0d58c38f0b97d9a41a72991707dc4ae7888c54d8
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-05 15:32:12 +0200

    Ajout de la page Promotion pour afficher les produits en promotion

---
+++ app/(client)/produits/promotion/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PromotionPage: React.FC = () => {
+  return (
+    <section>
+      <h1>Produits en promotion</h1>
+    </section>
+  );
+};
+
+export default PromotionPage;
