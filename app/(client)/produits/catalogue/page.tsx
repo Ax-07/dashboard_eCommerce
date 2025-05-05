f0bbdb25528eff8cea7c6f86bd90ebffcad15067
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-05 15:31:11 +0200

    Ajout de la page Catalogue pour afficher la liste des produits

---
+++ app/(client)/produits/catalogue/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CataloguePage: React.FC = () => {
+    return (
+        <div>
+            <h1>
+                Catalogue des produits
+            </h1>
+        </div>
+    );
+};
+
+export default CataloguePage;
