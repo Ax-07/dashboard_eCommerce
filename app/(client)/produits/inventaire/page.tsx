9cd42a871bd6bdb0abf9699bee9ace7f6f593f61
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-05 15:32:02 +0200

    Ajout de la page Inventaire pour afficher la liste des produits en stock

---
+++ app/(client)/produits/inventaire/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const InventairePage: React.FC = () => {
+    return (
+        <section>
+            <h1>
+                Inventaire des produits
+            </h1>
+        </section>
+    );
+};
+
+export default InventairePage;
