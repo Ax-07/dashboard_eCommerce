9376e6b989bfccb6c7ad23051c64911504ccb0d7
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-05 15:30:58 +0200

    Ajout de la page pour ajouter un produit avec un titre

---
+++ app/(client)/produits/ajouter/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AjouterPage: React.FC = () => {
+  return (
+    <section>
+      <h1>Ajouter un produit</h1>
+    </section>
+  );
+};
+
+export default AjouterPage;
