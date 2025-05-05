33e3d52244c8871f8b394f2ccb040b414928bcb0
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-05 15:32:19 +0200

    Ajout de la page Rupture pour afficher les produits en rupture de stock

---
+++ app/(client)/produits/rupture/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RupturePage: React.FC = () => {
+  return (
+    <section>
+      <h1>Produits en rupture de stock</h1>
+    </section>
+  );
+};
+
+export default RupturePage;
