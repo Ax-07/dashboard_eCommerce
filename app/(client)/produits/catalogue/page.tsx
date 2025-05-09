904c1f1f3287cc23eced1419954e609a5de4f477
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-09 10:56:28 +0200

    Refactor de la page Catalogue : intégration du composant Catalogue dans DashboardShell et suppression du contenu statique.

--- app/(client)/produits/catalogue/page.tsx
+++ app/(client)/produits/catalogue/page.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
+import Catalogue from "@/src/blocks/produits/catalogue/Catalogue";
+import { Skeleton } from "@/src/components/ui/skeleton";
+import { DashboardShell } from "@/src/blocks/dashboardBlocks/DashboardShell";
 
 const CataloguePage: React.FC = () => {
-    return (
-        <div>
-            <h1>
-                Catalogue des produits
-            </h1>
-        </div>
-    );
+  return (
+    <DashboardShell>
+      <div className="flex flex-col flex-1 p-4">
+        {/* <div className="grid grid-cols-3 gap-4 w-full h-40 mb-4">
+          <div className="bg-muted rounded-md"></div>
+          <div className="bg-muted rounded-md"></div>
+          <div className="bg-muted rounded-md"></div>
+        </div> */}
+        <Catalogue />
+      </div>
+    </DashboardShell>
+  );
 };
 
 export default CataloguePage;
