d91818091d5c5f361e58698de645392751fc0988
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-09 10:56:19 +0200

    Correction du chemin d'importation du composant AddProductForm pour assurer un chargement correct.

--- app/(client)/produits/ajouter/page.tsx
+++ app/(client)/produits/ajouter/page.tsx
@@ -1,5 +1,5 @@
-import AddProductForm from "@/src/blocks/produits/AddProductForm";
 import React from "react";
+import AddProductForm from "@/src/blocks/produits/addProduct/AddProductForm";
 
 const AjouterPage: React.FC = () => {
   return (
