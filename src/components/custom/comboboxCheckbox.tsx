34f4d309e7cb46d70c4b5acef5961bb359fa451d
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-10 10:43:58 +0200

    Ajout du composant ComboboxCheckbox avec gestion des options sélectionnables et intégration de l'interface utilisateur.

---
+++ src/components/custom/comboboxCheckbox.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Button } from "../ui/button";
+import {
+  Command,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "../ui/command";
+import { Checkbox } from "../ui/checkbox";
+
+interface ComboboxCheckboxProps {
+  options: string[]; // Liste des options à afficher dans le combobox
+  selectedOptions: string[]; // Liste des options sélectionnées
+  onSelect: (option: string) => void; // Fonction appelée lors de la sélection d'une option
+  placeholder?: string; // Texte d'espace réservé à afficher lorsque rien n'est sélectionné
+  label?: string; // Étiquette du combobox
+  icon?: React.ReactNode; // Icône à afficher à côté de l'étiquette
+  variant?: "link" | "default" | "destructive" | "outline" | "secondary" | "ghost" | null | undefined; // Variante du bouton (par exemple, "outline", "ghost", etc.)
+  size?: "default" | "icon" | "sm" | "lg" | null | undefined; // Taille du bouton (par exemple, "sm", "lg", etc.)
+}
+const ComboboxCheckbox: React.FC<ComboboxCheckboxProps> = ({
+  options,
+  selectedOptions,
+  onSelect,
+  placeholder,
+  label,
+  icon,
+  variant = "outline",
+  size = "default",
+}) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant={variant} size={size} className="">
+          {icon && <span className="">{icon}</span>}
+          {label && <span>{label}</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent align="start" className="w-[200px] p-0 border">
+        <Command>
+          <CommandInput placeholder={placeholder} className="h-9" />
+          <CommandList>
+            <CommandGroup>
+              {options.map((option) => (
+                <CommandItem
+                  key={option}
+                  onSelect={() => {
+                    onSelect(option);
+                  }}
+                  className="flex items-center"
+                >
+                  <Checkbox
+                    checked={selectedOptions.includes(option)}
+                    onCheckedChange={() => onSelect(option)}
+                    className="mr-2"
+                  />
+                  <span>{option}</span>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default ComboboxCheckbox;
