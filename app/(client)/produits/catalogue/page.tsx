fe2bb9abc415fec3ad06aaa5a543915dddb1973f
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-10 10:43:11 +0200

    Réorganisation de la structure de la page Catalogue en remplaçant un div par une section pour améliorer la sémantique et la lisibilité.

--- app/(client)/produits/catalogue/page.tsx
+++ app/(client)/produits/catalogue/page.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import Catalogue from "@/src/blocks/produits/catalogue/Catalogue";
-import { Skeleton } from "@/src/components/ui/skeleton";
 import { DashboardShell } from "@/src/blocks/dashboardBlocks/DashboardShell";
 
 const CataloguePage: React.FC = () => {
   return (
     <DashboardShell>
-      <div className="flex flex-col flex-1 p-4">
-        {/* <div className="grid grid-cols-3 gap-4 w-full h-40 mb-4">
-          <div className="bg-muted rounded-md"></div>
-          <div className="bg-muted rounded-md"></div>
-          <div className="bg-muted rounded-md"></div>
-        </div> */}
+      <section id="catalogue" className="relative flex-1 justify-between flex flex-col p-4">
         <Catalogue />
-      </div>
+      </section>
     </DashboardShell>
   );
 };
