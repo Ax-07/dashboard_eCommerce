9ca7aff7eb565629cecd56d2c40397c00a10251c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-11 17:03:46 +0200

    Correction de la couleur de la bordure pour le variant 'outline' du composant Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-xs hover:bg-destructive/90",
         outline:
-          "border border-input border-[hsl(120,100%,75%)]/50 bg-background shadow-xs hover:bg-gradient hover:text-accent-foreground",
+          "border border-input border-primary/50 bg-background shadow-xs hover:bg-gradient hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
