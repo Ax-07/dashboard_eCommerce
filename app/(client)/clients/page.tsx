cc6a10f55685a4e8852304eb30eb1fdfa249b9b6
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-13 19:01:26 +0200

    Ajout de la page Liste des clients

---
+++ app/(client)/clients/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListClientPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Liste des clients</h1>
+        </div>
+    );
+};
+
+export default ListClientPage;
