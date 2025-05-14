a16312fe5b00b2ca511d09631047fdedb5d31f15
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-14 11:42:19 +0200

    Ajout des commandes à la liste des commandes dans la page Commande

--- app/(client)/commandes/page.tsx
+++ app/(client)/commandes/page.tsx
@@ -1,12 +1,13 @@
 import { DashboardShell } from "@/src/blocks/dashboardBlocks/DashboardShell";
 import OrderList from "@/src/blocks/order/OrderList";
+import { ORDERS } from "@/src/mock";
 import React from "react";
 
 const CommandePage = () => {
   return (
     <DashboardShell>
       <section id="commandes" className="relative flex-1 justify-between flex flex-col p-4">
-        <OrderList />
+        <OrderList orders={ORDERS}/>
       </section>
     </DashboardShell>
   );
