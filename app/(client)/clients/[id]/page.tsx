976b68f9ef2514e6d9c052f82a299c1fcc488510
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-14 15:15:29 +0200

    Refactor la page ClientById pour intégrer le composant DashboardShell et afficher les détails de l'utilisateur.

--- app/(client)/clients/[id]/page.tsx
+++ app/(client)/clients/[id]/page.tsx
@@ -1,5 +1,8 @@
 // @/app/%28client%29/clients/%5Bid%5D/page.tsx
 
+import { DashboardShell } from '@/src/blocks/dashboardBlocks/DashboardShell';
+import UserDetails from '@/src/blocks/users/UserDetails';
+import { UserMock } from '@/src/mock/client.mock';
 import React from 'react';
 
 interface ClientByIdPageProps {
@@ -7,13 +10,15 @@ interface ClientByIdPageProps {
 }
 
 const ClientByIdPage: React.FC<ClientByIdPageProps> = ({params}) => {
-    const clientId = params.id; // Utiliser l'ID du client pour récupérer les détails du client
+    const userId = params.id; // Utiliser l'ID du client pour récupérer les détails du client
+        const user = UserMock.find((user) => user.id === userId); // Simuler la récupération des détails de l'utilisateur
+    
     return (
-        <div>
-            <h1>
-                Détails du client avec l'ID : {clientId}
-            </h1>
-        </div>
+    <DashboardShell>
+      <section id="catalogue" className="relative flex-1 justify-between flex flex-col p-4">
+        <UserDetails user={user} />
+      </section>
+    </DashboardShell>
     );
 };
 
