31f6ed945ef7f4df64ad13054d6780e7063b399c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-15 08:38:49 +0200

    Amélioration du formatage du composant ListClientPage en ajoutant des classes CSS manquantes et en réorganisant le code pour une meilleure lisibilité.

--- app/(client)/clients/page.tsx
+++ app/(client)/clients/page.tsx
@@ -1,16 +1,19 @@
-import { DashboardShell } from '@/src/blocks/dashboardBlocks/DashboardShell';
-import UsersList from '@/src/blocks/users/UsersList';
-import { UserMock } from '@/src/mock/client.mock';
-import React from 'react';
+import { DashboardShell } from "@/src/blocks/dashboardBlocks/DashboardShell";
+import UsersList from "@/src/blocks/users/UsersList";
+import { UserMock } from "@/src/mock/client.mock";
+import React from "react";
 
 const ListClientPage: React.FC = () => {
-    return (
+  return (
     <DashboardShell>
-      <section id="catalogue" className="relative flex-1 justify-between flex flex-col p-4">
-        <UsersList users={UserMock}/>
+      <section
+        id="catalogue"
+        className="relative flex-1 justify-between flex flex-col p-4 items-center"
+      >
+        <UsersList users={UserMock} />
       </section>
     </DashboardShell>
-    );
+  );
 };
 
 export default ListClientPage;
