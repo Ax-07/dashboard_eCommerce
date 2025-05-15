0cf3da5aac4792fac8da8a39068d789b240243d2
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-15 08:39:24 +0200

    Réorganisation du schéma de validation ReviewSchema pour améliorer la structure et la lisibilité du code.

--- src/lib/validators/review.zod.ts
+++ src/lib/validators/review.zod.ts
@@ -2,17 +2,20 @@ import { z } from "zod";
 
 export const ReviewSchema = z.object({
   id: z.string().uuid().optional(),
-    productId: z.string().uuid().optional(),
-    userId: z.string().uuid().optional(),
-    rating: z.number().min(1).max(5),
-})
+  productId: z.string().uuid().optional(),
+  productName: z.string().optional(),
+  comment: z.string().optional(),
+  createdAt: z.date().optional(),
+  userId: z.string().uuid().optional(),
+  rating: z.number().min(1).max(5),
+});
 
 export type ReviewInput = z.infer<typeof ReviewSchema>;
 export type ReviewOutput = z.infer<typeof ReviewSchema> & {
-    user: {
-        id: string;
-        name: string;
-        email: string;
-        image: string | null;
-    };
-    };
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    image: string | null;
+  };
+};
