177305d9013aa5a6ab7a4dc7b5052cba13e12b17
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-16 08:25:55 +0200

    Ajout de la page VenteCA avec le composant DashboardShell et une section catalogue.

---
+++ app/(client)/ventes/page.tsx
@@ -0,0 +1,16 @@
+import { DashboardShell } from '@/src/blocks/dashboardBlocks/DashboardShell';
+import React from 'react';
+
+const VenteCAPage = () => {
+    return (
+    <DashboardShell>
+      <section
+        id="catalogue"
+        className="relative flex-1 flex flex-col w-full p-4 items-center"
+      >
+      </section>
+    </DashboardShell>
+    );
+};
+
+export default VenteCAPage;
