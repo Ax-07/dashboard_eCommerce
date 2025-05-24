2edb0c47630f43a7e3be0da39698a415720e3c77
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-24 16:10:01 +0200

    Ajout de la récupération des données dans CAPage pour alimenter le composant StatistiqueVente.

--- app/(client)/ventes/ca/page.tsx
+++ app/(client)/ventes/ca/page.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import StatistiqueVente from "@/src/blocks/ventes/StatistiqueVente";
 import { DashboardShell } from "@/src/blocks/dashboardBlocks/DashboardShell";
+import { getTestGenerator } from "@/src/stores/actions/getTestGenerator";
 
 const CAPage = () => {
+ const data = getTestGenerator();
   return (
     <DashboardShell>
       <section
         id="catalogue"
         className="relative flex-1 flex flex-col w-full p-4 items-center"
       >
-        <StatistiqueVente />
+        <StatistiqueVente data={data}/>
       </section>
     </DashboardShell>
   );
