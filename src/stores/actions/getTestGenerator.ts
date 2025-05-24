ebd8467068e7e8f0d354d18b7111e2ba3f18d593
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-24 16:11:51 +0200

    Ajout de la fonction getTestGenerator pour récupérer les données des commandes via l'API.

---
+++ src/stores/actions/getTestGenerator.ts
@@ -0,0 +1,7 @@
+// @/src/stores/actions/getTestGenerator.ts
+export const getTestGenerator = async () => {
+  const base = "http://localhost:3000";
+  const res = await fetch(`${base}/api/orders`);
+  const data = await res.json(); 
+  return data;
+};
