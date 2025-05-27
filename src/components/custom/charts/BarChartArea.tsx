bddb59958d512dc13e5bc4fbe65bc284e6e68c90
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-05-27 12:53:52 +0200

    Ajout de la gestion des clés cachées dans le composant BarChartArea pour améliorer l'interaction utilisateur et la personnalisation des graphiques.

--- src/components/custom/charts/BarChartArea.tsx
+++ src/components/custom/charts/BarChartArea.tsx
@@ -5,24 +5,31 @@ import {
   ChartLegendContent,
   ChartTooltip,
   ChartTooltipContent,
+  CustomLegend,
   type ChartConfig,
 } from "@/src/components/ui/chart"
 import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts"
 import { ChartData } from "./useChartsDatas"
+import { useChartStore } from "@/src/stores/useChartStore"
 
 interface BarChartAreaProps {
+  id: string;
   chartData: ChartData[]
   chartConfig: ChartConfig
 }
 
 const BarChartArea: React.FC<BarChartAreaProps> = ({
+  id,
   chartData,
   chartConfig,
 }) => {
   const entries = Object.entries(chartConfig) as Array<
     [string, { label: string; color: string }]
   >
-
+  const stored = useChartStore(s => s.hiddenKeysByChart[id]);
+  const hiddenKeys = stored ?? [];  // fallback en local, hors selector
+  const toggleKey = useChartStore((s) => s.toggleKey);
+  
   return (
     <ChartContainer config={chartConfig}>
       <BarChart
@@ -49,7 +56,17 @@ const BarChartArea: React.FC<BarChartAreaProps> = ({
           }        
         />
         <ChartTooltip content={<ChartTooltipContent hideLabel />} />
-        <ChartLegend content={<ChartLegendContent />} />
+                    <ChartLegend
+                verticalAlign="bottom"
+                content={(props) => (
+                    <CustomLegend
+                      {...props}
+                      config={chartConfig}
+                      hiddenKeys={hiddenKeys}
+                      onToggleKey={(key) => toggleKey(id, key)}
+                    />
+                )}
+              />
 
         {entries.map(([key, { label, color }]) => (
           <Bar
@@ -58,6 +75,7 @@ const BarChartArea: React.FC<BarChartAreaProps> = ({
             name={label}
             fill={color}
             stackId="a"
+            hide={hiddenKeys.includes(key)} // ← on cache la barre si la clé est dans hiddenKeys
           />
         ))}
       </BarChart>
